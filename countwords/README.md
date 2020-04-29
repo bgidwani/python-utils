5c48f8e3d2187d640c40f02359b7d9b599217247
Bharat Gidwani <bgidwani@dgdean.com>
2020-04-28 20:54:32 -0400

    Updating count words to output max occurring word

--- countwords/README.md
+++ countwords/README.md
@@ -8,6 +8,4 @@ Sample code that can be used to count words in a text document. The project uses
 1. Replace `<FILE_NAME>` in `countwords.py` with path to a text document on your machine
 1. Run `python3 countwords.py` from terminal
 
-Once the process completes, the program will output number of empty and non-empty lines, along with word histogram extracted from the text document.
-
-The code assumes you have installed Python3 on your machine
+Once the process completes, the program will output number of empty and non-empty lines, along with max occurring word in the file. The word histogram can be printed by uncommenting the last print statement.
--- countwords/countwords.py
+++ countwords/countwords.py
@@ -1,7 +1,9 @@
 fhandle = open('<FILE_NAME>')
 emptylines = 0
 codelines = 0
+wordcount = 0
 counts = dict()
+maxOccur = ('', 0)
 for line in fhandle:
     strippedline = line.rstrip()
     if (strippedline == ''):
@@ -10,11 +12,18 @@ for line in fhandle:
     else:
         codelines += 1
     words = strippedline.split()
+    wordcount += len(words)
     for word in words:
         counts[word] = counts.get(word, 0) + 1
+        if (counts[word] > maxOccur[1]):
+            maxOccur = (word, counts[word])
     # print(words)
 
 fhandle.close()
+
+
 print('# of Empty lines = ' + str(emptylines))
 print('# of non Empty lines = ' + str(codelines))
-print('Word Histogram', counts)
+print('# of words = ' + str(wordcount))
+print('Max occurring word = ',  maxOccur)
+#print('Word Histogram', counts)
